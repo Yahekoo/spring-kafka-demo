06bd827ab5b50ea9cd2c1f0c207c42bed9727d55
yahekoo <zafin.dev@gmail.com>
2024-11-03 00:00:12 +0100

    Initial commit

---
+++ src/main/java/com/zedevia/spring_kafka_producer_consumer/SpringKafkaProducerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.zedevia.spring_kafka_producer_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaProducerConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaProducerConsumerApplication.class, args);
+	}
+
+}
